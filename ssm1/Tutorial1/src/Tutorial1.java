d81532edcda1700a2dacb20117ca9cc4c8195367
malkawimahdi <106409817+malkawimahdi@users.noreply.github.com>
2022-05-30 17:20:23 +0100

    Creation of ssm1.

---
+++ ssm1/Tutorial1/src/Tutorial1.java
@@ -0,0 +1,13 @@
+public class Tutorial1{
+
+    public static void main(String []args){
+        System.out.println("Hello World");
+
+        int x = 6;
+        int y = 13;
+
+        if (x < y) {
+            System.out.println(y); } else {
+            System.out.println(x); }
+    }
+}
